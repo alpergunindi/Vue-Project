6c77e8536109c551f40dcc80447f189036488401
Alper Günindi <alpergunindi@hotmail.com>
2023-07-18 14:20:50 +0200

    New Changes Made

---
+++ src/components/MainNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="w-full text-sm">Main Nav</header>
+  <div class="fixed left-0 top-0 h-16 w-full bg-white">
+    <div class="flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
+      <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+      <h2 class="ml-8 flex h-full items-center">
+        Developed by {{ author.firstName }} {{ author.lastName }}
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'Bobo Careers',
+      author: {
+        firstName: 'Alper',
+        lastName: 'Günindi'
+      }
+    }
+  }
+}
+</script>
---
+++ src/playground.js
@@ -0,0 +1,17 @@
+const developer = {
+  salary: 100000,
+  experience: 4.5
+  techStack: ["Vue", "HTML", "CSS"]
+  lookingForWork: true,
+  doubleSalary()  {
+    this
+  }
+}
+
+console.log(developer.salary);
+console.log(developer.lookingForWork);
+
+developer.doubleSalary();
+
+console.log(developer.salary);
+console.log(developer.lookingForWork);
