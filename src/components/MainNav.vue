6d27c9c02a21e7b69863ae9a2919909a8547d862
Alper Günindi <alpergunindi@hotmail.com>
2023-07-18 15:49:59 +0200

    Added a Nav Bar

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,10 +2,14 @@
   <header class="w-full text-sm">Main Nav</header>
   <div class="fixed left-0 top-0 h-16 w-full bg-white">
     <div class="flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-      <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
-      <h2 class="ml-8 flex h-full items-center">
-        Developed by {{ author.firstName }} {{ author.lastName }}
-      </h2>
+      <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+      <nav class="ml-12 h-full">
+        <ul class="flex h-full list-none">
+          <li v-for="menuItem in menuItems" :key="menuItem" class="ml-9 h-full">
+            <a href="" class="flex h-full items-center py-2.5">{{ menuItem }}</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
@@ -16,10 +20,8 @@ export default {
   data() {
     return {
       company: 'Bobo Careers',
-      author: {
-        firstName: 'Alper',
-        lastName: 'Günindi'
-      }
+      url: 'https://careers.google.com',
+      menuItems: ['Teams', 'Locations', 'Life at Bobo Corp', 'How We Hire', 'Students', 'Jobs']
     }
   }
 }
