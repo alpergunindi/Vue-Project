7df8a6f1116e0dd73db63c33e1cf9098ec656088
Alper Günindi <alpergunindi@hotmail.com>
2023-07-20 12:09:41 +0200

    New Features

---
+++ src/playground.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3, 4, 5]
+
+// an array of squares
+// [1, 4, 9, 16, 25]
+
+const squares = numbers.map((number) => return number * number)
+
+console.log(squares)
+
+const lowercaseNames = names.map((name) => {
+  return name.toLowerCase()
+})
+
+console.log(squares)
