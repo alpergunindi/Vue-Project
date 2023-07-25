d19b460968cc632eb6c5087c9b66b60861e056ea
Alper Günindi <alpergunindi@hotmail.com>
2023-07-25 11:37:37 +0200

    Added an Interval Method

--- src/playground.js
+++ src/playground.js
@@ -1,14 +1,7 @@
-const numbers = [1, 2, 3, 4, 5]
+setInterval(() => {
+  console.log('I will stop after 5 secs')
+}, 2000)
 
-// an array of squares
-// [1, 4, 9, 16, 25]
-
-const squares = numbers.map((number) => return number * number)
-
-console.log(squares)
-
-const lowercaseNames = names.map((name) => {
-  return name.toLowerCase()
-})
-
-console.log(squares)
+setTimeout(() => {
+  clear.Interval(Interval)
+}, 10000)
