0e167366730b935201453dfb15f37376837bc1e0
Alper Günindi <alpergunindi@hotmail.com>
2023-07-25 10:36:50 +0200

    Made new features

---
+++ src/components/TheHero.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <section class="flex h-screen flex-col bg-yellow-200 pb-20 pt-10">I can be your hero!</section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'TheHero'
+}
+</script>
