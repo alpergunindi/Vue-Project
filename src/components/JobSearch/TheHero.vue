c570e26ca4b18ef5f8c7a2e38de5ae2ceb42fa67
Alper Günindi <alpergunindi@hotmail.com>
2023-07-26 11:59:36 +0200

    Add JobSearchForm

--- src/components/JobSearch/TheHero.vue
+++ src/components/JobSearch/TheHero.vue
@@ -6,7 +6,12 @@
 
         <div class="col-span-5 col-start-2"><the-headline /><job-search-form /></div>
 
-        <div class="col-span-5 col-start-7"></div>
+        <div class="self-center- col-span-5 col-start-7 justify-self-center">
+          <img
+            class="border- rounded-s-full border-r-8 border-brand-blue-2"
+            src="https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2021/04/29101948/change-careers-1024x512.png"
+          />
+        </div>
 
         <div class="col-span-1 col-start-12"></div>
       </div>
@@ -15,8 +20,8 @@
 </template>
 
 <script>
-import TheHeadline from '@/components/TheHeadline.vue'
-import JobSearchForm from '@/components/JobSearchForm.vue'
+import TheHeadline from '@/components/JobSearch/TheHeadline.vue'
+import JobSearchForm from '@/components/JobSearch/JobSearchForm.vue'
 
 export default {
   name: 'TheHero',
--- src/components/JobSearchForm.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <form>Bobo Corps takes wing!</form>
-</template>
-
-<script>
-export default {
-  name: 'JobSearchForm'
-}
-</script>
